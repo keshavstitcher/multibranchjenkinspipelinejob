4caf687141b785257ace6569a5f9ab27082da11a
keshavstitcher <78870478+keshavstitcher@users.noreply.github.com>
2022-03-16 22:49:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo yum install -y unzip
 unzip awscliv2.zip
 sudo ./aws/install
